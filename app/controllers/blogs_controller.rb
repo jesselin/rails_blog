744fc776e1b8d4c55dac9c57ebd17094b56cd7e3
Jesse Lin <jesselin@gmail.com>
2013-10-01 18:38:08 -0700

    delete post from blog show

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -26,10 +26,7 @@ class BlogsController < ApplicationController
 
   def show
     set_blog
-
     @posts = Post.all.where("blog_id = ?", set_blog.id).order("created_at DESC")
-    # @post_owner =
-    # @blog_owner = 
   end
 
   def edit
